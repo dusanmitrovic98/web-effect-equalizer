98395c5a37e238733945f7cc6311ed276ac2e701
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-18 23:13:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const bars = document.querySelectorAll(".bar");
 // Function to update the visualizer bars based on audio input
 function updateVisualizer(amplitude) {
   const maxHeight = 50; // Maximum bar height
+  const barHeight = amplitude * maxHeight;
