123c7857f36e7448f9c284e0d2c52f68df3415b1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-18 23:17:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ function simulateVoiceInput() {
   const maxAmplitude = 1; // Adjust this value based on the input sensitivity
   const randomAmplitude = Math.random() * maxAmplitude;
   updateVisualizer(randomAmplitude);
+}
